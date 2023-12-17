4e0eb5479ab325db641fd05c7c0f5201c1ea983b
ervanervan <ervanervan040@gmail.com>
2023-12-17 17:30:25 +0700

    Update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 function Footer() {
   return (
     <footer className="sm:px-16 py-5 px-8 bg-[#161921]">
-      <p className="text-base font-bold text-white">@2023 Anime Movies</p>
+      <p className="text-base font-bold text-white">2023 - Anime Movies</p>
     </footer>
   );
 }
